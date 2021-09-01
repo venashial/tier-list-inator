2785dd09341d6593c6aa545dbb88223f3b4d3d8c
venashial <venashial.levo@aleeas.com>
2021-09-01 01:38:26 -0700

    Close strings

--- src/routes/tierlist/[id].svelte
+++ src/routes/tierlist/[id].svelte
@@ -1,6 +1,6 @@
 <script>
-import List from './_List.svelte;
-import Panel from './_Panel.svelte;
+import List from './_List.svelte';
+import Panel from './_Panel.svelte';
 </script>
 
 <div class="grid grid-cols-list h-full">
